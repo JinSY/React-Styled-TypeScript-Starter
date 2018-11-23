067c57fe5bce8e1625b40a3742bbe4a0771c3ebd
Paul Yoon <pyoon@twg.io>
2018-11-23 10:36:50 -0500

    precommit hooks

--- src/__test__/foo.test.tsx
+++ src/__test__/foo.test.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from 'react'
+import { shallow } from 'enzyme'
 
-import App from '../app';
+import App from '../app'
 
 test('test', () => {
-  expect(true).toBeTruthy();
-});
+  expect(true).toBeTruthy()
+})
 
 test('snapshot', () => {
-  const result = shallow(<App />);
+  const result = shallow(<App />)
 
-  expect(result).toMatchSnapshot();
+  expect(result).toMatchSnapshot()
 })
